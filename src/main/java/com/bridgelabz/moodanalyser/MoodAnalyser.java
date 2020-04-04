0bbab8a6288776e478e9651af18affaf8d569e30
rajkush211 <rajkush211.rk@gmail.com>
2020-04-04 06:49:17 +0530

    REFACTORED : Code to take Mood message in Constructor

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,8 +1,14 @@
 package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser {
+
+    String message;
+    // Parameterized Constructor
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
     // Method for analyzing the mood
-    public String analyseMood(String message) {
+    public String analyseMood() {
         if (message.contains("Sad"))
             return "Sad";
         else
--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -4,25 +4,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
-
-    MoodAnalyser moodAnalyser = new MoodAnalyser();
     // Test for testing Sad mood
     @Test
     public void givenMessage_whenSad_shouldReturnSad() {
-        String mood = moodAnalyser.analyseMood("Today I am Sad");
-        Assert.assertEquals("Sad", mood);
+        MoodAnalyser moodAnalyser = new MoodAnalyser("Today I am Sad");
+        Assert.assertEquals("Sad", moodAnalyser.analyseMood());
     }
     // Test for testing Happy mood
     @Test
     public void givenMessage_whenHappy_shouldReturnHappy() {
-        String mood = moodAnalyser.analyseMood("Today I am Happy");
-        Assert.assertEquals("Happy", mood);
+        MoodAnalyser moodAnalyser = new MoodAnalyser("Today I am Happy");
+        Assert.assertEquals("Happy", moodAnalyser.analyseMood());
     }
     // Test for testing Any other mood
-
     @Test
     public void givenMessage_whenAny_shouldReturnHappy() {
-        String mood = moodAnalyser.analyseMood("I am in Any Mood");
-        Assert.assertEquals("Happy", mood);
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any Mood");
+        Assert.assertEquals("Happy", moodAnalyser.analyseMood());
     }
 }
