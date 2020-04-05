46eeb4cfdea71c52703c29d569c1c3e6ee74750a
rajkush211 <rajkush211.rk@gmail.com>
2020-04-05 08:09:36 +0530

    ADDED : Code to use Reflection to invoke method

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyserFactory.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyserFactory.java
@@ -2,6 +2,7 @@ package com.bridgelabz.moodanalyser;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MoodAnalyserFactory {
     // Default Constructor
@@ -18,7 +19,18 @@ public class MoodAnalyserFactory {
         }
     }
 
-    public static MoodAnalyser createMoodAnalyserObject(Constructor<?> moodAnalyserConstructor, Object... message) throws IllegalAccessException, InvocationTargetException, InstantiationException {
-        return (MoodAnalyser)moodAnalyserConstructor.newInstance(message);
+    public static MoodAnalyser createMoodAnalyserObject(Constructor<?> moodAnalyserConstructor, Object... moodAnalyserObject) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        return (MoodAnalyser)moodAnalyserConstructor.newInstance(moodAnalyserObject);
+    }
+
+    public static Object invokeMethod(Object moodAnalyserObject, String analyseMood) throws MoodAnalysisException, IllegalAccessException, InvocationTargetException {
+        try {
+            Class objectClass = moodAnalyserObject.getClass();
+            Method moodMethod = objectClass.getMethod(analyseMood);
+            Object result = moodMethod.invoke(moodAnalyserObject);
+            return result;
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD, "Method not found");
+        }
     }
 }
